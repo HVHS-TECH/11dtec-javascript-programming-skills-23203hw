1798fffa0c276b885b21005aff11d919be6c4aa2
23203hw <23203hw@hvhs.school.nz>
2025-05-12 21:59:28 +0000

    12345

--- t03_maths.js
+++ t03_maths.js
@@ -8,23 +8,25 @@ console.log("math ");
 
 //variables
 let name="Hollie"
-let age=15
+let age=16
 let pocketmoney= 10000000
 let year =2025
-let birthyear =year -age
-var mynumber =15
+let birthyear =year - age
+var mynumber =16
+var ageplusten =age+10
 var myothernumber =2
 /*
 main code
 */
 console.log("as of " , year , "you are" , age , "years old")
 console.log("you were born in" , birthyear)
-*/
+
 console.log("Hi" , name )
 console.log("as of " , year , "you are" , age , "years old")
 console.log("you were born in" , birthyear)
 console.log ("you have" , pocketmoney, "dollars") 
-console.log ("in 10 years you will be " )
+console.log ("in 10 years you will be " +ageplusten +"years old ")
+console.log ("half of your money is" ,pocketmoney/2)
 
 /*
 functions
