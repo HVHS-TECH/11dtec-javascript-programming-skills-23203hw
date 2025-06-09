536a05fc62fee35aaf72f4169fa15eead59c46e2
23203hw <23203hw@hvhs.school.nz>
2025-06-09 21:59:50 +0000

    ghjgcfghjfjhfjhfgh

--- t12_while_loop.js
+++ t12_while_loop.js
@@ -16,3 +16,9 @@ main code
 /*
 functions
 */
+var password = prompt ("speak 'friend' and enter");
+while ( password != "friend"){ 
+password = prompt ("that's not the password.\nspeak 'friend'and enter");
+}
+alert("welcome friend! ")
+
---
+++ t13_validation.js
@@ -0,0 +1,26 @@
+/*
+task 1 introduction.js 
+hollie 
+29/04/2025 
+week 1 
+*/
+console.log("hiii people how are you today .js");
+
+//variables
+
+/*
+main code
+*/
+function start(){
+    getfavcolour();
+}
+
+
+/*
+functions
+*/
+function  getfavcolour () {
+    userfavcolour = prompt("what is your favourite colour?")
+    alert("your favourite colour is: "+ userfavcolour)
+}
+    
